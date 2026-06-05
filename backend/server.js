072a91cfd4f8682eee273f4285dbafefd0f54f1b
Abhilasha Lahare <abhilashalahare6@gmail.com>
2026-06-05 15:57:15 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ console.log(process.env.MONGO_URI);
 import playerRoutes from './routes/playerRoutes.js';
 
 const app = express();
-app.use(cors());
+app.use(cors(),
+allowedOrigins = [ 'https://cricket-form-lac.vercel.app'],
+
+);
 app.use(express.json({ limit: '10mb' })); 
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
